bd33ac1517208561bf0f9eebe792d813a0a8e49d
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-04-11 22:12:29 +0500

    feat: add interfaces IOrder, IOrderSuccess, IOrderError

--- src/types/index.ts
+++ src/types/index.ts
@@ -12,3 +12,24 @@ interface IProduct {
   price: number | null; // Цена продукта (может быть null)
 }
 
+interface IProductNotFound {
+  error: string;        // Сообщение об ошибке ("NotFound")
+}
+
+interface IOrder {
+  payment: string;      // Способ оплаты ("online")
+  email: string;        // Электронная почта
+  phone: string;        // Телефон
+  address: string;      // Адрес доставки
+  total: number;        // Общая сумма заказа
+  items: string[];      // Массив идентификаторов продуктов
+}
+
+interface IOrderSuccess {
+  id: string;           // Уникальный идентификатор заказа
+  total: number;        // Общая сумма заказа
+}
+
+interface IOrderError {
+  error: string;        // Сообщение об ошибке
+}
