7fac30ddd7ba3dedde489749de2142ac5e641ff7
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-04-27 03:47:23 +0500

    feat:add the AppData.ts file

---
+++ src/components/AppData.ts
@@ -0,0 +1,16 @@
+import { Model } from './base/Model';
+import { IProduct, IOrder, IAppState } from '../types';
+/**
+ * Модель для управления продуктами.
+ */
+export class ProductModel extends Model<IProduct> {}
+
+/**
+ * Модель для управления заказами.
+ */
+export class OrderModel extends Model<IOrder> {}
+
+/**
+ * Модель для состояния приложения.
+ */
+export class AppState extends Model<IAppState> {}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,37 @@
 export interface IProductList {
-  total: number;        // Общее количество продуктов
-  items: IProduct[];    // Массив продуктов
+	total: number; // Общее количество продуктов
+	items: IProduct[]; // Массив продуктов
 }
 
 export interface IProduct {
-  id: string;           // Уникальный идентификатор продукта
-  description: string;  // Описание продукта
-  image: string;        // Путь к изображению продукта
-  title: string;        // Название продукта
-  category: string;     // Категория продукта
-  price: number | null; // Цена продукта (может быть null)
+	id: string; // Уникальный идентификатор продукта
+	description: string; // Описание продукта
+	image: string; // Путь к изображению продукта
+	title: string; // Название продукта
+	category: string; // Категория продукта
+	price: number | null; // Цена продукта (может быть null)
 }
 
 export interface IProductNotFound {
-  error: string;        // Сообщение об ошибке ("NotFound")
+	error: string; // Сообщение об ошибке ("NotFound")
 }
 
 export interface IOrder {
-  payment: string;      // Способ оплаты ("online")
-  email: string;        // Электронная почта
-  phone: string;        // Телефон
-  address: string;      // Адрес доставки
-  total: number;        // Общая сумма заказа
-  items: string[];      // Массив идентификаторов продуктов
+	payment: string; // Способ оплаты ("online")
+	email: string; // Электронная почта
+	phone: string; // Телефон
+	address: string; // Адрес доставки
+	total: number; // Общая сумма заказа
+	items: string[]; // Массив идентификаторов продуктов
 }
 
 export interface IOrderSuccess {
-  id: string;           // Уникальный идентификатор заказа
-  total: number;        // Общая сумма заказа
+	id: string; // Уникальный идентификатор заказа
+	total: number; // Общая сумма заказа
 }
 
 export interface IOrderError {
-  error: string;        // Сообщение об ошибке
+	error: string; // Сообщение об ошибке
 }
+
+export interface IAppState {}
