bd8f4f86f43d9d0ebf09ca5b1950c095b856ac10
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-04-27 20:47:00 +0500

    feat: add the Order class

--- src/components/Order.ts
+++ src/components/Order.ts
@@ -1,1 +1,28 @@
+import { Form } from './common/Form';
+import { IOrder } from '../types';
+import { EventEmitter } from './base/Events';
+import { IFormState } from '../types';
 
+export class Order extends Form<IOrder> {
+	constructor(container: HTMLFormElement, events: EventEmitter) {
+		super(container, events);
+	}
+
+	render(state: Partial<IOrder> & IFormState): HTMLFormElement {
+		const { address, payment, errors } = state;
+
+		// Устанавливаем валидность формы
+		this.valid = !!address && !!payment;
+
+		// Вызываем родительский метод render
+		return super.render({
+			...state,
+			valid: this.valid,
+			errors: errors || [],
+		});
+	}
+
+	isValid(): boolean {
+		return this.valid;
+	}
+}
