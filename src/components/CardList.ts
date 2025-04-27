756de916d4f957ea6c24efae30419ecfe60cd4d9
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-04-27 20:16:05 +0500

    feat: add the CardList class

---
+++ src/components/CardList.ts
@@ -0,0 +1,34 @@
+import { Component } from './base/Component';
+import { IEvents } from './base/Events';
+import { IProductList } from '../types';
+import { Card } from './Card';
+
+export class CardList extends Component<IProductList> {
+	private items: Card[] = [];
+	private events: IEvents;
+
+	constructor(container: HTMLElement, events: IEvents) {
+		super(container);
+		this.events = events; // Сохраняем events локально
+	}
+
+	render(data?: Partial<IProductList>): HTMLElement {
+		const products = data?.items || [];
+
+		// Очищаем контейнер с помощью replaceChildren
+		this.container.replaceChildren();
+
+		this.items = products.map((product) => {
+			const template = document.getElementById(
+				'card-catalog'
+			) as HTMLTemplateElement;
+			const cardElement = template.content.cloneNode(true) as HTMLElement;
+			const card = new Card(cardElement, this.events);
+			card.render(product);
+			this.container.append(cardElement);
+			return card;
+		});
+
+		return this.container; // Возвращаем контейнер для совместимости с базовым классом
+	}
+}
