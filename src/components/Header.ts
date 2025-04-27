22f031185284620da07fdc0e6e7852c3b2ca2f0d
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-04-27 20:19:46 +0500

    feat: add the Header class

--- src/components/Header.ts
+++ src/components/Header.ts
@@ -1,1 +1,29 @@
+import { Component } from './base/Component';
+import { IEvents } from './base/Events';
 
+export class Header extends Component<{}> {
+	private basketCounter: HTMLElement;
+	private events: IEvents;
+
+	constructor(container: HTMLElement, events: IEvents) {
+		super(container);
+		this.events = events; // Сохраняем events локально
+		this.basketCounter = this.container.querySelector(
+			'.header__basket-counter'
+		)!;
+		this.initBasketButton();
+	}
+
+	private initBasketButton(): void {
+		const basketButton = this.container.querySelector(
+			'.header__basket'
+		) as HTMLButtonElement;
+		basketButton.addEventListener('click', () => {
+			this.events.emit('basket:open');
+		});
+	}
+
+	updateBasketCounter(count: number): void {
+		this.basketCounter.textContent = count.toString();
+	}
+}
